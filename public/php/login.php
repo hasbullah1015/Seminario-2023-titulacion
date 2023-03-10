eb40cc539c88772123a38806fe3203207ca445d1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-09 22:08:31 -0600

    ok

--- public/php/login.php
+++ public/php/login.php
@@ -1,17 +1,6 @@
 <?php
-
-    $dbhost = "127.0.0.1";
-    $dbuser = "u614989997_admindb";
-    $dbpswd = "6TUH72toIDh5RpVGBi";
-    $dbname = "u614989997_db_inventario";
-
-    $connection= mysqli_connect($dbhost,$dbuser,$dbpswd,$dbname);
-    if(!$connection){
-
-        die("no se logro hacer la conexión con la db admin: ".mysqli_error());
-    }
-
-
+    include("databaseconnection.php");
+    $connection=conectar();
     $user = $_POST['user'];
     $pswd = $_POST['password'];
     $sqlquery =mysqli_query($connection,"SELECT * FROM  administracion WHERE usr = '$user' AND pass ='$pswd'");
