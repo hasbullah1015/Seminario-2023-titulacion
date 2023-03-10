35666f337c125a02d99b284474508781f5d30f19
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-09 22:30:38 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -5,9 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>inventario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </head>
 
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                hola
+            </div>
+        </div>
+    </div>
+
+
     <?php
         echo 'gg';
         include("databaseconnection.php");
