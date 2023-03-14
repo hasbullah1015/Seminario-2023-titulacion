1d49e425930317b69e36f90a14bd0480483068f6
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-13 20:01:56 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -29,7 +29,7 @@
     
                 if(isset($_POST['enviar'])){
                     $busqueda = $POST['busqueda'];
-                    $sqlquery =mysqli_query($connection,"SELECT * FROM producto WHERE categoria LIKE '.%$busqueda%.' OR idProducto LIKE '.%$busqueda%.' OR nombre LIKE '.%$busqueda%.'");
+                    $sqlquery =mysqli_query($connection,"SELECT * FROM producto WHERE categoria LIKE '%$busqueda%' OR idProducto LIKE '%$busqueda%' OR nombre LIKE '%$busqueda%'");
                 }
 
             ?>
