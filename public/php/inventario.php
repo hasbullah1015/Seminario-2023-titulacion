b046532174172b09bbf31c08997da8a872d497f1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-14 23:23:37 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -84,8 +84,8 @@
             <div class="col">
                 <ul>
                     <li><a href="#"><<</a></li>
-                    <?php for($i=1; $i<$total_paginas; $i++){
-                        echo '<li><a href="?pagina='.$i.'&busueda='.$aux.'">'.$i.'</a></li>';
+                    <?php for($i=0; $i<$total_paginas; $i++){
+                        echo '<li><a href="?pagina='.$i.'&busueda='.$aux.'">'.($i+1).'</a></li>';
                     } ?>
 
                     <li><a href=">>"><<</a></li>
