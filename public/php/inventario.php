9f1a7b30fe250e92ae80b464d0abbfeab44cc657
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-14 21:04:03 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -19,7 +19,7 @@
         <div class="row">
             <h1>Inventario: </h1> <hr id="div-line">
             <div class="row">
-                <form   method="post">
+                <form method="post">
                     <input type="text" placeholder="busqueda" name="busqueda"> <!-- id, producto, marca--> 
                     <input type="submit" name="enviar" value="buscar">
                 </form>
