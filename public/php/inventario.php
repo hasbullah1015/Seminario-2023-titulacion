ec954a7cbe058ebbeb7bef3909ab28cbba9a78d9
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-15 08:41:29 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -7,7 +7,7 @@
         $busqueda=strtolower($_REQUEST['busqueda']);
         if(empty($busqueda)){
             header('https://seminario2023.website/public/php/inventario.php');
-            mysqli_close($connection);
+       
         }
 
 
