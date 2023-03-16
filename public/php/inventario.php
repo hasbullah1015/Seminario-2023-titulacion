e9c92ce25465f5a5e91d63290374b585fc30525c
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-15 21:09:47 -0600

    Update inventario.php

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -26,9 +26,9 @@
 </head>
 
 <body>
+<div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
     <header id="encabezado">
     </header>
-<div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
     <div class="container">
         <div class="row">
             <h1>Inventario: </h1> <hr id="div-line">
