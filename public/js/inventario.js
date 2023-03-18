da78e577db326e9f3a4570305b223e628241d22a
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-18 13:26:24 -0600

    Update inventario.js

--- public/js/inventario.js
+++ public/js/inventario.js
@@ -1,5 +1,5 @@
-const row_data = document.querySelectorAll("row_data");
-const data = document.querySelectorAll("data");
+const row_data = document.querySelectorAll(".row_data");
+const data = document.querySelectorAll(".data");
 
 for (i=0; i<row_data.length; i++){
     console.log(data[i].innerHTML);
