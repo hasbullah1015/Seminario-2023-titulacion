b9a0a6fdb9d1b297bc95fbb483bf8e73b7c43649
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-18 13:05:36 -0600

    ok

--- public/php/inventario.php
+++ public/php/inventario.php
@@ -21,7 +21,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link rel="stylesheet" href="/public/css/inventario.css" type="text/css">
-    <script src="/public/js/inventario.js"></script>
+    <script src="/public/js/index.js"></script>
 </head>
 
 <body>
@@ -95,9 +95,8 @@
     </div>
     <br>
     <footer id="pie_pagina" class="mt-auto">
-
     </footer>
-    <script src="/public/js/index.js"></script>
+    <script src="/public/js/inventario.js"></script>
 </body>
 </html>
 
