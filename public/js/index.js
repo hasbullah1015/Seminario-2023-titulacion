0e750b1878bedc7f2763bff98500b5428e5e3c59
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-21 20:55:31 -0600

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,4 @@
 $("#pie_pagina").load("/public/html/footer.html");
 $("#encabezado").load("/public/html/header.html");
 $("#main_data").load("/public/html/userdata.php");
-$("#db_data_panel").load("/public/php/inventario.php");
+
