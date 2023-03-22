b33cffc1fedd351db48d68b13a62d114c9847c88
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-21 23:46:49 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ $("#encabezado").load("/public/html/header.html");
 
 const photo = document.querySelector("#userPortrait");
 const user = document.querySelector("#userLabel");
-let url = "public_html/public/assets_img"+user.innerHTML;
+let url = "public_html/public/assets_img/"+user.innerHTML+".jpg"; //concatenamos la url con el usuario
 
 console.log(url);
 
