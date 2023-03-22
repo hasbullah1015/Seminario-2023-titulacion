b020763a8fbe69fe1b23a0f5b24bae25abbe2b93
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-21 23:40:28 -0600

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,12 @@
 $("#pie_pagina").load("/public/html/footer.html");
 $("#encabezado").load("/public/html/header.html");
 
+const photo = document.querySelector("#userPortrait");
+const user = document.querySelector("#userLabel");
+let url = "public_html/public/assets_img"+user.innerHTML;
+
+console.log(url);
+
+
 
 
