19290a716a467b676b062d26cb4c2251ac2456d3
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-21 21:39:45 -0600

    ok

--- public/html/index_panel.php
+++ public/html/index_panel.php
@@ -1,3 +1,8 @@
+<?php 
+    session_start();
+    $_SESSION['user']=$_user;
+?>
+
 <!DOCTYPE html>
 <html lang=”en”> 
 <head>
--- public/php/login.php
+++ public/php/login.php
@@ -5,10 +5,10 @@
     $user = $_POST['user'];
     $pswd = $_POST['password'];
     session_start(); //crear inicio de sesion
-    $_SESSION['user']=$user; //crear la sesión del ususario
     
     $sqlquery =mysqli_query($connection,"SELECT * FROM  administracion WHERE usr = '$user' AND pass ='$pswd'"); //realizar la validación en la db mediante la conexioón
     if($sqlquery->num_rows > 0){ //se realizá la validación (si hay al menos un usuario)
+        $_SESSION['user']=$user; //crear la sesión del ususario
         header('Location: https://seminario2023.website/public/html/index_panel.html');
 
     }
