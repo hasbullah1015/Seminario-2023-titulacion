50b599a7c516e7c568e9834f87b91b4fc14e2a79
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-22 23:13:21 -0600

    Update index_panel.php

--- public/php/index_panel.php
+++ public/php/index_panel.php
@@ -26,6 +26,12 @@
         <div class="row">
             <div class="col-sm-12 col-md-4 col-lg-4">
                 <h2>Datos Personales</h2>
+                <div>
+                    hola
+                    <div>
+                        holax2
+                    </div>
+                </div>
                 
             </div>
 
