0b3e3a26e8c25d9d788fd273041ba6babe0310c1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-24 17:54:34 -0600

    Update product_manager.php

--- public/php/product_manager.php
+++ public/php/product_manager.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/product_manager.css" type="text/css">
     <title>Gestor de producto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
 </head>
+
 <body>
     <div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
     <header id="encabezado"></header>
