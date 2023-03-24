0bffc85a57e24d73912ec58e24b206d61b3b03ae
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-24 17:42:54 -0600

    ok

---
+++ public/php/product_manager.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/product_manager.css" type="text/css">
+    <title>Gestor de producto</title>
+</head>
+<body>
+    <div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
+    <header id="encabezado"></header>
+    <div class="container">
+        <div class="row">
+            <h1>Gestor de productos:</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
