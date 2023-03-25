b9eb014a4bd8aee51bd09d89eb7d2a671b6e230b
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-24 22:59:43 -0600

    Update product_manager.php

--- public/php/product_manager.php
+++ public/php/product_manager.php
@@ -40,7 +40,11 @@
                     <input type="text" id="search_bar" name="busqueda" placeholder="busqueda">
                     <input type="submit" id="sent" name="enviar">
                 </form>
-
+                <?php 
+                    $busqueda=strtolower($_REQUEST['busqueda']);
+                    $sqlquery_busqueda=mysqli_query($connection,"SELECT * FROM producto WHERE id LIKE '%$busqueda%' OR nombre LIKE %$busqueda%");
+                    $resultado=mysqli_num_rows($sqlquery_busqueda);
+                ?>
             </div>
             <div class="slide col-sm-12 col-md-8" id="data_product">
                     <div>ID</div>
