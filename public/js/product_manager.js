49ffb0fab1b3ebfa5c87bbf85121b1c3cf7b6811
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-25 12:39:42 -0600

    Update product_manager.js

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -2,7 +2,7 @@ const product_id = document.querySelector("#product_id");
 const module = document.querySelector("#qr_form_mod");
 
 
-if(product_id.innerHTML == ""){
+if(!!product_id){
     module.classList.add("slide");
     module.innerHTML="p";
 }
