d6c907609bcde8e8a8dc4dfcffa27ee082753cd1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-26 15:51:33 -0600

    ok

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -1,25 +1,25 @@
 
 const sent = document.querySelector("#sent");
 
-sent.addEventListener("click",qr_form_generator)
+sent.addEventListener("click",qr_form_generator);
 
 
 function qr_form_generator(){
-    const search_Bar = document.querySelector("#search_bar"); 
+    const search_panel = document.querySelector("#search_panel"); 
     const product_id = document.querySelector("#product_id");
     const module = document.querySelector("#qr_form_mod");
 
-    console.log("producto: "+product_id.innerHTML)
 
     if(product_id.innerHTML != 'n/d'){
         module.classList.add("slide");
-        search_Bar.classList.add("dataWarning")
         module.innerHTML="qr";
 
     }
     else{   
         module.classList.add("slide");
         module.innerHTML="form";
+        search_panel.classList.add("dataWarning");
+
     }
 }
 
--- public/php/product_manager.php
+++ public/php/product_manager.php
@@ -36,7 +36,7 @@
             <h1>Gestor de productos:</h1>
         </div>
         <div class="row">
-            <div class="slide col-sm-12 col-md-4">
+            <div class="slide col-sm-12 col-md-4" id="search_panel">
                 <form action="product_manager.php" method="get">
                     <input type="text" id="search_bar" name="busqueda" placeholder="busqueda">  
                     <input type="submit" id="sent" name="enviar">
