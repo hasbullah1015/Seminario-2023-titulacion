f0b42facfbb56bd8b8d1545f8ee06d0a496303f9
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-26 15:19:15 -0600

    ok

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -2,14 +2,14 @@ const product_id = document.querySelector("#product_id");
 const module = document.querySelector("#qr_form_mod");
 console.log("producto: "+product_id.innerHTML)
 
-if(product_id.innerHTML == "n/d"){
-    module.classList.add("slide");
-    module.innerHTML="form";
-}
-else{ 
+if(product_id.innerHTML != "n/d"){
     module.classList.add("slide");
     module.innerHTML="qr";
 
+}
+else{ 
+    module.classList.add("slide");
+    module.innerHTML="form";
 }
 
 //añadir clase slide
