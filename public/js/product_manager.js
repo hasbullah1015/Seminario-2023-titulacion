782b5769d89c673c1a9a7526dba1f10a6229e9cf
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-27 22:26:28 -0600

    Update product_manager.js

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -8,14 +8,49 @@ if(product_id.innerHTML != 'n/d'){
         module.classList.add("slide");
         new QRCode(QR_module, product_id.innerHTML); //generacíon de qr
         form_module.innerHTML="gola";
- 
+    
 }
 else{   
         module.classList.add("slide");
-        //module.innerHTML="form";
         module.classList.add("dataWarning");
+        form_generator();
 }
 
+
+function form_generator(){
+    const new_form = document.createElement("form");
+    const id_input = document.createElement("input");
+    const name_input=document.createElement("input");
+    const category = document.createElement("input");
+    const submit = document.createElement("input");
+
+
+    //asignación de atributos
+    new_form.setAttribute('method',"get");
+    new_form.setAttribute('action',"");
+
+    id_input.setAttribute('type',"text");
+    id_input.setAttribute('placeholder',"inserte id");
+
+    name_input.setAttribute('type',"text");
+    name_input.setAttribute('placeholder',"inserte nombre");
+
+    category.setAttribute('type',"text");
+    category.setAttribute('placeholder',"inserte nombre");
+
+    submit.setAttribute('type',"button");
+
+
+    new_form.appendChild(id_input);
+    new_form.appendChild(name_input);
+    new_form.appendChild(category);
+    new_form.appendChild(submit);
+    form_module.appendChild(new_form);
+
+
+
+
+}
 
 
 //añadir clase slide
