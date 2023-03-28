0f4fb06b7f66d36befecf6b5bcd917702d377809
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-27 21:21:16 -0600

    Update product_manager.js

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -1,13 +1,12 @@
 
 const product_id = document.querySelector("#product_id");
 const module = document.querySelector("#qr_form_mod");
-
-
+const QR = new QRcode (module);
 
 if(product_id.innerHTML != 'n/d'){
         module.classList.add("slide");
         module.innerHTML="qr";
-        module.innerHTML = new QRcode(module,product_id.innerHTML); //Generamos el codigo QR 
+        QR.makeCode(product_id.link.value);
 }
 else{   
         module.classList.add("slide");
