7140186190aab271ab42571dae5c01e145de14f0
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-27 22:01:50 -0600

    Update product_manager.js

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -1,12 +1,13 @@
 
 const product_id = document.querySelector("#product_id");
+const module = document.querySelector("#qr_form_mod");
 const QR_module = document.querySelector("#qr_mod");
 const form_module = document.querySelector("#form_mod");
-const module = document.querySelector("#qr_form_mod");
 
 if(product_id.innerHTML != 'n/d'){
         module.classList.add("slide");
         new QRCode(QR_module, product_id.innerHTML); //generacíon de qr
+        form_module.innerHTML="gola";
  
 }
 else{   
