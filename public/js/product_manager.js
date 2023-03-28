33152c71d444243020906e50dcaa33197dfedfd1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-03-27 21:25:16 -0600

    Update product_manager.js

--- public/js/product_manager.js
+++ public/js/product_manager.js
@@ -1,12 +1,12 @@
 
 const product_id = document.querySelector("#product_id");
 const module = document.querySelector("#qr_form_mod");
-const QR = new QRcode (module);
 
 if(product_id.innerHTML != 'n/d'){
         module.classList.add("slide");
         module.innerHTML="qr";
-        QR.makeCode(product_id.link.value);
+        new QRCode(product_id, "http://jindo.dev.naver.com/collie");
+
 }
 else{   
         module.classList.add("slide");
