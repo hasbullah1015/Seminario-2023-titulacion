fdf73182ea1f3dfe9b8d20fc69aa17e61c889dc5
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-19 21:31:49 -0600

    Update db_action.php

--- public/php/db_action.php
+++ public/php/db_action.php
@@ -22,7 +22,7 @@ if($id && $nombre && $categoria && $opc){
             $drop_result=mysqli_num_rows( $sqlquery_consulta);
 
             if( $drop_result>0){
-               // $mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria'");
+                mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria'");
                 echo 'Se eliminó el producto';
             }
             else echo 'No se eliminó el producto, datos erroneos';
