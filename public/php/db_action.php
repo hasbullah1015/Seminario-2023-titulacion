e3568477276c6bd98ae7571a2aa600d652d2a257
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-19 21:04:12 -0600

    Update db_action.php

--- public/php/db_action.php
+++ public/php/db_action.php
@@ -5,21 +5,28 @@ $categoria=strtolower($_REQUEST['categoria']);
 $opc=strtolower($_REQUEST['opc']);
 
 if($id && $nombre && $categoria && $opc){
+    $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id'");
+    $resultado=mysqli_num_rows($sqlquery_consulta);
 
-    if($opc=="insert"){
-        $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id'");
-        $resultado=mysqli_num_rows($sqlquery_consulta);
+    if($opc=="insert"){    
         if($resultado>0){
-            echo 'Ya existe un producto con el id:';
-           
+            echo 'Ya existe un producto con el id:';  
         }
         else echo 'insert';
         
     }
+    
     if($opc=="drop"){
-        echo 'drop';
+        if($resultado>0){
+            mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria'");
+        echo 'Se eliminó el producto';
+        }
     }
 }
+
+
+
+
 else{
     echo 'Ingresa los datos completos: ';
 }
