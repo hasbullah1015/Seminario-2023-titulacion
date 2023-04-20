81208b1b30b2ef294d8c76c2c7c39b31f22e6118
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-19 20:34:22 -0600

    oki

--- public/php/db_action.php
+++ public/php/db_action.php
@@ -1,10 +1,18 @@
 <?php
+$id=strtolower($_REQUEST['id']);
+$nombre=strtolower($_REQUEST['nombre']);
+$categoria=strtolower($_REQUEST['categoria']);
 $opc=strtolower($_REQUEST['opc']);
 
-if($opc=="insert"){
-    echo 'insert';
+if($id && $nombre && $categoria && $opc){
+    if($opc=="insert"){
+        echo 'insert';
+    }
+    if($opc=="drop"){
+        echo 'drop';
+    }
 }
-if($opc=="drop"){
-    echo 'drop';
+else{
+    echo 'Ingresa los datos completos: ';
 }
 ?>
