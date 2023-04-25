ee6d66f7234d641bf94c2910de31cebf37ede23b
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-24 23:05:30 -0600

    Update general_view_json.php

--- public/php/general_view_json.php
+++ public/php/general_view_json.php
@@ -19,6 +19,15 @@
 
         if(strtolower($_REQUEST['enviar'])=='critical'){
             echo 'adio locoooo';
+            $sqlquery_registros_crtcl =mysqli_query($connection,"SELECT * FROM producto  WHERE cantidad BETWEEN 0 AND 10 ORDER BY ASC");
+            $cantidad_registros=mysqli_num_rows($sqlquery_registros_crtcl);
+            if($cantidad_registros>0)
+            {
+                while($datos = $sqlquery_registros_crtcl ->fetch_array(MYSQLI_ASSOC)){
+                    echo json_encode($datos);
+                }
+
+            }
         }
     
 
