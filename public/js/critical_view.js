33375a6f24f890a299ac9f7adfd2c3e906ebec69
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 20:12:12 -0600

    Update critical_view.js

--- public/js/critical_view.js
+++ public/js/critical_view.js
@@ -11,7 +11,8 @@ var myChart = new Chart(ctx, {
         datasets: [{
             label: 'Stock de Productos',
             backgroundColor: ['#6bf1ab','#63d69f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1'],
-            borderColor: ['black'],
+            borderColor: ['black'], 
+            data: [12,10,20,10],
             borderWidth:1
         }]
     },
@@ -24,19 +25,4 @@ var myChart = new Chart(ctx, {
     }
 })
 
-let url = 'https://seminario2023.website/public/php/general_view_json.php?enviar=general'
 
-fetch(url)
-    .then( response => response.json() )
-    .then( datos => mostrar(datos) )
-    .catch( error => console.log(error) )
-
-
-const mostrar = (articulos) =>{
-    articulos.forEach(element => {
-        myChart.data['labels'].push(element.nombre)
-        myChart.data['datasets'][0].data.push(element.cantidad)
-        myChart.update()
-    });
-    console.log(myChart.data)
-}    
