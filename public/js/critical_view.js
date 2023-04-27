527af3392f293b91efcd849bc3bf6005ec04f22a
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 21:58:54 -0600

    Update critical_view.js

--- public/js/critical_view.js
+++ public/js/critical_view.js
@@ -1,44 +1,46 @@
 
-const btn_general_view=document.querySelector("#general_view");
-const btn_critical_view=document.querySelector("#critical_view");
+const btn_general=document.querySelector("#general_view");
+const btn_critical=document.querySelector("#critical_view");
 
+btn_general.addEventListener("click",general_view);
 
-
-var ctx = document.querySelector(".slide #myChart");
-var myChart = new Chart(ctx, {
-    type:'bar',
-    data:{
-        datasets: [{
-            label: 'Stock de Productos',
-            backgroundColor: ['#6bf1ab','#63d69f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1'],
-            borderColor: ['black'], 
-            borderWidth:1
-        }]
-    },
-    options:{
-        scales:{
-            y:{
-                beginAtZero:true
+function general_view(){
+    var ctx = document.querySelector(".slide #myChart");
+    var myChart = new Chart(ctx, {
+        type:'bar',
+        data:{
+            datasets: [{
+                label: 'Stock de Productos',
+                backgroundColor: ['#6bf1ab','#63d69f', '#438c6c', '#509c7f', '#1f794e', '#34444c', '#90CAF9', '#64B5F6', '#42A5F5', '#2196F3', '#0D47A1'],
+                borderColor: ['black'], 
+                borderWidth:1
+            }]
+        },
+        options:{
+            scales:{
+                y:{
+                    beginAtZero:true
+                }
             }
         }
-    }
-})
+    })
 
 
 
-let url = 'https://seminario2023.website/public/php/general_view_json.php'
+    let url = 'https://seminario2023.website/public/php/general_view_json.php'
 
-fetch(url)
-    .then( response => response.json() )
-    .then( datos => mostrar(datos) )
-    .catch( error => console.log(error) )
+    fetch(url)
+        .then( response => response.json() )
+        .then( datos => mostrar(datos) )
+        .catch( error => console.log(error) )
 
 
-const mostrar = (articulos) =>{
-    articulos.forEach(element => {
-        myChart.data['labels'].push(element.nombre)
-        myChart.data['datasets'][0].data.push(element.cantidad)
-        myChart.update()
-    });
-    console.log(myChart.data)
-}    
+    const mostrar = (articulos) =>{
+        articulos.forEach(element => {
+            myChart.data['labels'].push(element.nombre)
+            myChart.data['datasets'][0].data.push(element.cantidad)
+            myChart.update()
+        });
+        console.log(myChart.data)
+    }    
+}
