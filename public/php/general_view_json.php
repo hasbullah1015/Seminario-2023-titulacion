9c6e518bb6c05f287a44302e68f5a8b1ff0d638b
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 21:25:22 -0600

    Update general_view_json.php

--- public/php/general_view_json.php
+++ public/php/general_view_json.php
@@ -1,10 +1,5 @@
 <?php
 
-            $sqlquery_registros_gral =mysqli_query($connection,"SELECT * FROM producto");
-            return $sqlquery_registros_gral -> fetch_all(MYSQL_ASSOC);
-
-        
-
 
 
 ?>
