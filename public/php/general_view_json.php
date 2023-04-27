7f87f6ccf87d3ddb3d9d6e3a488e813990d7b8b6
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 21:00:51 -0600

    Update general_view_json.php

--- public/php/general_view_json.php
+++ public/php/general_view_json.php
@@ -4,18 +4,12 @@
 
 
 
-        if(strtolower($_REQUEST['enviar'])=='general'){
             $sqlquery_registros_gral =mysqli_query($connection,"SELECT * FROM producto ");
             $datos = mysqli_fetch_all($sqlquery_registros_gral,MYSQL_ASSOC);
             echo json_encode($datos);
-        }
+        
 
-        if(strtolower($_REQUEST['enviar'])=='critical'){
-            $sqlquery_registros_crtcl =mysqli_query($connection,"SELECT * FROM producto  WHERE cantidad BETWEEN 0 AND 10 ORDER BY cantidad ASC");
-            $datos = mysqli_fetch_all($sqlquery_registros_crtcl,MYSQL_ASSOC);
-            echo json_encode($datos);
-        }
-    
+
 
 
 
