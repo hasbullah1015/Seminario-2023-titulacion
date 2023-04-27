16c866cafe42a21a311f418b66c73e8ab1bd342b
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 20:53:54 -0600

    Update general_view_json.php

--- public/php/general_view_json.php
+++ public/php/general_view_json.php
@@ -6,26 +6,14 @@
 
         if(strtolower($_REQUEST['enviar'])=='general'){
             $sqlquery_registros_gral =mysqli_query($connection,"SELECT * FROM producto ");
-            $cantidad_registros=mysqli_num_rows($sqlquery_registros_gral);
-            if($cantidad_registros>0)
-            {
-                while($datos = $sqlquery_registros_gral ->fetch_array(MYSQLI_ASSOC)){
-                    echo json_encode($datos);
-                }
-
-            }
+            $datos = mysqli_fetch_all($sqlquery_registros_gral,MYSQL_ASSOC);
+            echo json_encode($datos);
         }
 
         if(strtolower($_REQUEST['enviar'])=='critical'){
             $sqlquery_registros_crtcl =mysqli_query($connection,"SELECT * FROM producto  WHERE cantidad BETWEEN 0 AND 10 ORDER BY cantidad ASC");
-            $cantidad_registros=mysqli_num_rows($sqlquery_registros_crtcl);
-            if($cantidad_registros>0)
-            {
-                while($datos = $sqlquery_registros_crtcl ->fetch_array(MYSQLI_ASSOC)){
-                    echo json_encode($datos);
-                }
-
-            }
+            $datos = mysqli_fetch_all($sqlquery_registros_crtcl,MYSQL_ASSOC);
+            echo json_encode($datos);
         }
     
 
