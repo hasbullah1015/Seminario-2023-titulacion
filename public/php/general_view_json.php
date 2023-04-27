cb2004d0eb344c5a990b45964bb546b60fe19b06
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-04-26 21:33:22 -0600

    Update general_view_json.php

--- public/php/general_view_json.php
+++ public/php/general_view_json.php
@@ -1,10 +1,14 @@
 <?php
     include("databaseconnection.php");
     $connection=conectar();
+
     $sqlquery_registros_gral =mysqli_query($connection,"SELECT * FROM producto");
-
-        
-
+    $valores = array();
+    
+    while ($data = mysqli_fetch_assoc($sqlquery_registros_gral)){
+        $valores[]=$data;
+    }
+    echo json_encode($valores);
 
 
 ?>
