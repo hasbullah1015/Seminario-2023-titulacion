813bf51cbdd4ca7468d8ddc819a1a771a3be423c
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-08 21:23:04 -0600

    Update critical_view.js

--- public/js/critical_view.js
+++ public/js/critical_view.js
@@ -6,6 +6,48 @@ btn_general.addEventListener("click",general_view);
 btn_critical=addEventListener("click", critical_view);
 
 
+
+function critical_view(){
+    var ctx = document.querySelector(".slide #myChart");
+    var myChart = new Chart(ctx, {
+        type:'bar',
+        data:{
+            datasets: [{
+                label: 'Stock de Productos',
+                backgroundColor: ['#0E52FC','#FC0E36', '#0EFC61'],
+                borderColor: ['black'], 
+                borderWidth:1
+            }]
+        },
+        options:{
+            scales:{
+                y:{
+                    beginAtZero:true
+                }
+            }
+        }
+    })
+
+    let url = 'https://seminario2023.website/public/php/general_view_json.php'
+
+    fetch(url)
+        .then( response => response.json() )
+        .then( datos => mostrar(datos) )
+        .catch( error => console.log(error) )
+
+    const mostrar = (articulos) =>{
+        articulos.forEach(element => {
+            myChart.data['labels'].push(element.nombre)
+            myChart.data['datasets'][0].data.push(element.total_salida)
+            myChart.update()
+        });
+        //console.log(myChart.data)
+    }    
+}
+
+
+
+
 function general_view(){
     var ctx = document.querySelector(".slide #myChart");
     var myChart = new Chart(ctx, {
@@ -46,41 +88,4 @@ function general_view(){
 
 
 
-function critical_view(){
-    var ctx = document.querySelector(".slide #myChart");
-    var myChart = new Chart(ctx, {
-        type:'bar',
-        data:{
-            datasets: [{
-                label: 'Stock de Productos',
-                backgroundColor: ['#0E52FC','#FC0E36', '#0EFC61'],
-                borderColor: ['black'], 
-                borderWidth:1
-            }]
-        },
-        options:{
-            scales:{
-                y:{
-                    beginAtZero:true
-                }
-            }
-        }
-    })
-
-    let url = 'https://seminario2023.website/public/php/general_view_json.php'
-
-    fetch(url)
-        .then( response => response.json() )
-        .then( datos => mostrar(datos) )
-        .catch( error => console.log(error) )
-
-    const mostrar = (articulos) =>{
-        articulos.forEach(element => {
-            myChart.data['labels'].push(element.nombre)
-            myChart.data['datasets'][0].data.push(element.total_salida)
-            myChart.update()
-        });
-        //console.log(myChart.data)
-    }    
-}
 
