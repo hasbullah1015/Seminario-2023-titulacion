c4876299990493ec96e88457da9f70dfd4ca9543
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-09 21:36:52 -0600

    ok

--- public/php/index_panel.php
+++ public/php/index_panel.php
@@ -71,7 +71,7 @@
     <footer id="pie_pagina" class="mt-auto">
     </footer>
     <script src="/public/js/index.js"></script>
-    <script type="module" src="/public/js/critical_view.js"></script>
+    <script type="module" src="/public/js/panels_view.js"></script>
 
 
 </body>
