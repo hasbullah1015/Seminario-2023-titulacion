a23e11fd3b00b16fa2aa057dc9b59add87d09651
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-10 21:50:47 -0600

    Update panels_view.js

--- public/js/panels_view.js
+++ public/js/panels_view.js
@@ -11,25 +11,26 @@ let url = 'https://seminario2023.website/public/php/general_view_json.php';
 
 function draw_chart_entrada(){
     const ctx = document.querySelector(".slide #myChart");
-    var myChart = new Chart(ctx, {
-        type:'bar',
-        data:{
-            datasets: [{
-                label: 'Stock de Productos',
-                backgroundColor: ['#0C134F','#1D267D', '#5C469C','#D4ADFC','#3A1078','#4E31AA','#2F58CD','#3795BD'],
-                borderColor: ['black'], 
-                borderWidth:1
-            }]
-        },
-        options:{
-            scales:{
-                y:{
-                    beginAtZero:true
+    if(!myChart){ 
+        var myChart = new Chart(ctx, {
+            type:'doughnut',
+            data:{
+                datasets: [{
+                    label: 'Stock de Productos',
+                    backgroundColor: ['#0C134F','#1D267D', '#5C469C','#D4ADFC','#3A1078','#4E31AA','#2F58CD','#3795BD'],
+                    borderColor: ['black'], 
+                    borderWidth:1
+                }]
+            },
+            options:{
+                scales:{
+                    y:{
+                        beginAtZero:true
+                    }
                 }
             }
-        }
-    })
-
+        })
+    }
     fetch(url)
         .then( response => response.json() )
         .then( datos => mostrar(datos) )
@@ -48,24 +49,28 @@ function draw_chart_entrada(){
 
 function draw_chart_salida(){
     const ctx = document.querySelector(".slide #myChart");
-    var myChart2 = new Chart(ctx, {
-        type:'doughnut',
-        data:{
-            datasets: [{
-                label: 'Stock de Productos',
-                backgroundColor: ['#0C134F','#1D267D', '#5C469C','#D4ADFC','#3A1078','#4E31AA','#2F58CD','#3795BD'],
-                borderColor: ['black'], 
-                borderWidth:1
-            }]
-        },
-        options:{
-            scales:{
-                y:{
-                    beginAtZero:true
+    
+    
+    if(!myChart2){ 
+        var myChart2 = new Chart(ctx, {
+            type:'doughnut',
+            data:{
+                datasets: [{
+                    label: 'Stock de Productos',
+                    backgroundColor: ['#0C134F','#1D267D', '#5C469C','#D4ADFC','#3A1078','#4E31AA','#2F58CD','#3795BD'],
+                    borderColor: ['black'], 
+                    borderWidth:1
+                }]
+            },
+            options:{
+                scales:{
+                    y:{
+                        beginAtZero:true
+                    }
                 }
             }
-        }
-    })
+        })
+    }
 
     fetch(url)
         .then( response => response.json() )
