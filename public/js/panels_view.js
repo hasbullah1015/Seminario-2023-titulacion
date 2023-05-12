c19da947df32112ae4ae751688fb54d96c68f6f7
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-11 23:48:04 -0600

    ok

--- public/js/panels_view.js
+++ public/js/panels_view.js
@@ -62,7 +62,7 @@ function draw_chart_salida(){
         data:{
             datasets: [{
                 label: 'Stock de Salida',
-                backgroundColor: ['#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#c501e2','#9a27f7','#ff0571'],
+                backgroundColor: ['#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602'],
                 borderRadius: 30,
                 borderSkipped: false,
             }]
