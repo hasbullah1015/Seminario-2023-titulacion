752220c536739adc5ed4db1b042205d5ca8dcd13
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-12 00:12:08 -0600

    Update panels_view.js

--- public/js/panels_view.js
+++ public/js/panels_view.js
@@ -26,7 +26,7 @@ function draw_chart_entrada(){
                 backgroundColor: ['#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6','#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6',],
                 borderColor: ['black'], 
                 borderWidth:1,
-                borderRadius: {topLeft: 10, topRight:10 }
+                borderRadius: {topLeft: 10, topRight:10 },
             }]
         },
         options:{
