7802d9259f8f957ed7a85e0cb9f8891ff4b9e172
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-12 00:22:13 -0600

    Update panels_view.js

--- public/js/panels_view.js
+++ public/js/panels_view.js
@@ -26,7 +26,7 @@ function draw_chart_entrada(){
                 backgroundColor: ['#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6','#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6',],
                 borderColor: ['black'], 
                 borderWidth:1,
-                borderRadius: {topLeft: 5, topRight:5 },
+                borderRadius: {topLeft: , topRight:5 },
             }]
         },
         options:{
@@ -64,11 +64,8 @@ function draw_chart_salida(){
             datasets: [{
                 label: 'Stock de Salida',
                 backgroundColor: ['#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6','#2b97fa','#c501e2','#2ef8a0', '#e7c500','#f82d97','#e830ce','#a7fd2a','#9a27f7','#ff0571','#ff0534','#c6e602','#00fff6',],
-                borderRadius: {topLeft: 5, topRight:5 },
                 borderWidth:1,
-                borderRadius: {topLeft: 5, topRight:5 },
-            }]
-            }]
+                borderRadius: {topLeft: , topRight:5 },
             }]
         },
         options:{
