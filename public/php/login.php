bd6e8833e0d3d78bcd74378ca6e96e68f912c2ac
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-13 10:59:34 -0600

    Update login.php

--- public/php/login.php
+++ public/php/login.php
@@ -5,6 +5,8 @@
     $pswd = $_POST['password'];
     
     $sqlquery =mysqli_query($connection,"SELECT * FROM  administracion WHERE usr = '$user' AND pass ='$pswd'"); //realizar la validación en la db mediante la conexioón
+   
+
     if($sqlquery->num_rows > 0){ //se realizá la validación (si hay al menos un usuario)
         session_start(); //crear inicio de sesion
         $_SESSION['username']=$user; //se guarda el usuario en la variable de tipo sesion 
@@ -16,9 +18,8 @@
 
     }
     else{
-        header('https://seminario2023.website/index.php');
-?>
-<?php
+        header('https://seminario2023.website');
+
     }
     mysqli_close($connection);
 ?>
