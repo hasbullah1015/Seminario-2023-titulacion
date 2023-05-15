9955e8fb4ee976bd89bbfa2a85c20ed5de49b7cc
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-14 21:07:46 -0600

    Update index_panel.php

--- public/php/index_panel.php
+++ public/php/index_panel.php
@@ -18,10 +18,10 @@
     <meta charset="utf-8">
     <title>FACE ID WEBSITE</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link rel="stylesheet" href="/public/css/index.css" type="text/css">
     <link rel="stylesheet" href="/public/css/header_style.css" type="text/css">
     <link rel="stylesheet" href="/public/css/footer_style.css" type="text/css">
