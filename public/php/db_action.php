ad53904ea4a698a0a4dd40f8d9f6eb5b9c5fe275
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-24 23:14:14 -0600

    Update db_action.php

--- public/php/db_action.php
+++ public/php/db_action.php
@@ -1,55 +1,43 @@
-<?php 
-    session_start();
+<?php
+$id=strtolower($_REQUEST['id']);
+$nombre=strtolower($_REQUEST['nombre']);
+$categoria=strtolower($_REQUEST['categoria']);
+$brand=strtolower($_REQUEST['marca']);
+$opc=strtolower($_REQUEST['opc']);
+$user=$_SESSION['username'];
+$id=$_SESSION['id'];
+$rol=$_SESSION['rol'];
 
-    if (empty($_SESSION["username"])) {
-        header('Location: https://seminario2023.website/index.php');
+if($id && $nombre && $categoria && $opc && $brand){
+    $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id'");
+    $resultado=mysqli_num_rows($sqlquery_consulta);
+
+    if($opc=="insert"){    
+        if($resultado>0){
+            echo 'Ya existe un producto con el id:';  
+        }
+        else echo 'Se añadió el producto';
+        mysqli_query($connection,"INSERT INTO producto ( idProducto, nombre, categoria, marca, cantidad ) values ('$id', '$nombre', '$categoria','$brand' ,'0')");
+        
+    }
+    
+    if($opc=="drop"){
+        if($resultado>0){
+            $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria' AND marca = '$brand'");
+            $drop_result=mysqli_num_rows( $sqlquery_consulta);
+
+            if( $drop_result>0){
+                mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria' AND marca = '$brand'");
+                echo 'Se eliminó el producto';
+            }
+            else echo 'No se eliminó el producto, datos erroneos';
+        }
+        else echo 'No existe un producto con ese ID';
 
     }
+}
 
-    else{
-        include("databaseconnection.php");
-        $connection=conectar();
-        $user=$_SESSION['username'];
-        $rol=$_SESSION['rol'];
-    
-
-        $id=strtolower($_REQUEST['id']);
-        $nombre=strtolower($_REQUEST['nombre']);
-        $categoria=strtolower($_REQUEST['categoria']);
-        $brand=strtolower($_REQUEST['marca']);
-        $opc=strtolower($_REQUEST['opc']);
-
-        if($id && $nombre && $categoria && $opc && $brand){
-            $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id'");
-            $resultado=mysqli_num_rows($sqlquery_consulta);
-
-            if($opc=="insert"){    
-                if($resultado>0){
-                    echo 'Ya existe un producto con el id:';  
-                }
-                else echo 'Se añadió el producto';
-                mysqli_query($connection,"INSERT INTO producto ( idProducto, nombre, categoria, marca, cantidad ) values ('$id', '$nombre', '$categoria','$brand' ,'0')");
-                
-            }
-            
-            if($opc=="drop"){
-                if($resultado>0){
-                    $sqlquery_consulta=mysqli_query($connection,"SELECT idProducto FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria' AND marca = '$brand'");
-                    $drop_result=mysqli_num_rows( $sqlquery_consulta);
-
-                    if( $drop_result>0){
-                        mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id' AND nombre = '$nombre' AND categoria = '$categoria' AND marca = '$brand'");
-                        echo 'Se eliminó el producto';
-                    }
-                    else echo 'No se eliminó el producto, datos erroneos';
-                }
-                else echo  $user.' No existe un producto con ese ID';
-
-            }
-        }
-
-        else{
-            echo $user. 'Ingresa los datos completos: ';
-        }
+else{
+    echo 'Ingresa los datos completos: ';
 }
 ?>
