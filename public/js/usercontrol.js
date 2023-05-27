8ded4418979592e3f32203770207bae6f233db79
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-27 11:34:55 -0600

    Update usercontrol.js

--- public/js/usercontrol.js
+++ public/js/usercontrol.js
@@ -1,16 +1,28 @@
-const form =  document.querySelector("#form-section");
-const data = document.querySelector("#data-section");
+const formSection =  document.querySelector("#form-section");
+const dataSection = document.querySelector("#data-section");
 const typeUser = document.querySelector("#typeUser").innerHTML;
 
 form_generator(typeUser);
 
 function form_generator(typeUser){
 
-    if(typeUser==="(b)"){
+    if(typeUser==="(a)"){
+        const newForm=document.createElement("form");
+        const id = document.createElement("input");
+        const name = document.querySelector("input");
+        const rol = document.querySelector("input");
+        const  insertOpc = document.createElement("input");
+        const dropOpc = document.createElement("input");
+        const label1 = document.createElement("label");
+        label1.innerHTML="Añadir usuario";
+        const label2 = document.createElement("label");
+        label2.innerHTML="Eliminar usuario";
+
+
 
     }
     else{
-        form.innerHTML='No disponible';
-        data.innerHTML='Ponte en contacto con un administrador para la gestión de usuarios';
+        formSection.innerHTML='No disponible';
+        dataSection.innerHTML='Ponte en contacto con un administrador para la gestión de usuarios';
     }
 }
