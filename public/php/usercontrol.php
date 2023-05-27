9ac5ee7c2a7b44505f98026e06a2adf2a114d8b6
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-26 21:28:06 -0600

    ok

---
+++ public/php/usercontrol.php
@@ -0,0 +1,62 @@
+<?php 
+    session_start();
+
+    if (empty($_SESSION["username"])) {
+        header('Location: https://seminario2023.website/index.php');
+
+    }
+    else{
+        include("databaseconnection.php");
+        $connection=conectar();
+        $user=$_SESSION['username'];
+        $rol=$_SESSION['rol'];
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Control de usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" href="/public/css/inventario.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/header_style.css" type="text/css">
+    <link rel="stylesheet" href="/public/css/footer_style.css" type="text/css">
+
+</head>
+
+<body>
+<div id="video-background" ><video src="/public/assets_img/renderizado-26011.mp4" autoplay="true" muted="true" loop="true" ></video> </div>
+    <header id="encabezado">
+    </header>
+    <div class="container">
+        <div class="row">
+            <h1>Control de usuarios: </h1> <hr id="div-line"  style="margin: 0px 0px 10px;">
+
+
+            <div  class="slide">
+                <div id="form-section" style="col-md-6 col-sm-12">
+                <span> Bienvenido <?php echo $user ?> ( <?php echo $rol ?>)
+                </div>
+                <div id="data-seccion" style="col-md-6 col-sm-12"></div>
+
+
+            </div>
+
+        </div>
+    </div>
+    <br>
+    <footer id="pie_pagina" class="mt-auto"></footer>
+    
+<script src="/public/js/inventario.js"></script>
+<script src="/public/js/index.js"></script>
+</body>
+</html>
+
+ 
