c3b3339f8869060bece333caee738d14acbf8d41
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-26 22:54:45 -0600

    ok

--- public/js/usercontrol.js
+++ public/js/usercontrol.js
@@ -1,3 +1,15 @@
 const form =  document.querySelector("#form-section");
 const data = document.querySelector("#data-section");
-const typeUser = document.querySelector("#typeUser");z
+const typeUser = document.querySelector("#typeUser").innerHTML;
+
+
+function form_generator(typeUser){
+
+    if(typeUser==="(b)"){
+
+    }
+    else{
+        form.innerHTML='No disponible';
+        data.innerHTML='Ponte en contacto con un administrador para la gestión de usuarios';
+    }
+}
