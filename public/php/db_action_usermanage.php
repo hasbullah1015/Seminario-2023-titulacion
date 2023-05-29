199ff22ddeec35758a875fa16da1d7b6c047aae1
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-28 22:38:50 -0600

    Update db_action_usermanage.php

--- public/php/db_action_usermanage.php
+++ public/php/db_action_usermanage.php
@@ -5,7 +5,7 @@ $id=strtoupper($_REQUEST['id']);
 
 
     if($opc =="insert"){
-        $sqlquery_consulta=mysqli_query($connection,"SELECT id FROM usuarios WHERE id = '$id'");
+        $sqlquery_consulta=mysqli_query($connection,"SELECT idUser FROM administracion WHERE idUser = '$id'");
         if(mysqli_num_rows($sqlquery_consulta)>0)
         {
             echo 'Ya existe un usuario con ese ID, favor de ingresar uno nuevo';
