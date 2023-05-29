4723e750c9ae326eeca7c1b860b4a54563e68bec
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-29 00:40:21 -0600

    Update db_action_usermanage.php

--- public/php/db_action_usermanage.php
+++ public/php/db_action_usermanage.php
@@ -6,13 +6,19 @@ $id=strtoupper($_REQUEST['id']);
 
     if($opc =="insert"){
         $user=strtoupper($_REQUEST['user']);
+        $_rol=strtoupper($_REQUEST['rol']);
+        $pswd = $_REQUEST['pswd'];
+
         $sqlquery_consulta=mysqli_query($connection,"SELECT idUser FROM administracion WHERE idUser = '$id'");
         if(mysqli_num_rows($sqlquery_consulta)>0)
         {
             echo 'Ya existe un usuario con ese ID, favor de ingresar uno nuevo';
         }
         else {
-            $sqlquery_consulta=mysqli_query($connection,"INSERT INTO administracion (idUser, usr, pass, rol) values ('$id', '$user', '$pswd', '$rol')");
+            $sqlquery_consulta=mysqli_query($connection,"INSERT INTO administracion (idUser, usr, pass, rol) values ('$id', '$user', '$pswd', '$_rol')");
+
+            echo 'Usuario agregado correctamente';
+
 
 
         }
