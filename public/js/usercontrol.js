d85ae606583c26920e10a2f9b0ba6221b22eeb5e
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-29 00:15:55 -0600

    Update usercontrol.js

--- public/js/usercontrol.js
+++ public/js/usercontrol.js
@@ -72,7 +72,9 @@ function form_generator(typeUser){
         function  insert_user (){
 
             text.appendChild(opcText);
-            text.setAttribute("disabled selected");
+            text.setAttribute("disabled");
+            text.setAttribute("selected");
+
 
             opc1.setAttribute("name","rol");
             opc1.setAttribute("value","A");
