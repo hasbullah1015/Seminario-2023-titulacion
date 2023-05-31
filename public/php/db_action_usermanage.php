430cd93e69d8d38bbd8fd0c6b0b36e603e635a79
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-30 21:33:15 -0600

    Update db_action_usermanage.php

--- public/php/db_action_usermanage.php
+++ public/php/db_action_usermanage.php
@@ -24,7 +24,10 @@ if($opc && $id){
     $sqlquery_consulta=mysqli_query($connection,"SELECT idUser FROM administracion WHERE idUser = '$id'");
     if(mysqli_num_rows($sqlquery_consulta)>0)
     {
-        echo 'Ya existe un usuario con ese ID, favor de ingresar uno nuevo';
+        $sqlquery_consulta=mysqli_query($connection,"DELETE FROM administracion WHERE  idUser ='$id'");
+        echo 'Usuario Eliminado correctamente';
+
+
     }
     else {
 
