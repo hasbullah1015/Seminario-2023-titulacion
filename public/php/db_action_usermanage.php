34b5deffaf191b4796756c362342d88fe256cd26
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-30 21:48:06 -0600

    Update db_action_usermanage.php

--- public/php/db_action_usermanage.php
+++ public/php/db_action_usermanage.php
@@ -6,9 +6,8 @@ $user=strtoupper($_REQUEST['user']);
 $_rol=strtoupper($_REQUEST['rol']);
 $pswd = $_REQUEST['pswd'];
 
-
+echo 'Ingresa los datos solicitados para la gestión de usuarios';
 if($opc=='insert' && $id && $user && $_rol  && $pswd){
-    echo 'Ingresa los datos solicitados para la gestión de usuarios';
         $sqlquery_consulta=mysqli_query($connection,"SELECT idUser FROM administracion WHERE idUser = '$id'");
         if(mysqli_num_rows($sqlquery_consulta)>0)
         {
