715ec7fb999a959f522a55af33ef89f52244e58f
hasbullah1015 <126923993+hasbullah1015@users.noreply.github.com>
2023-05-31 18:03:17 -0600

    Update db_action.php

--- public/php/db_action.php
+++ public/php/db_action.php
@@ -13,13 +13,13 @@ $resultado=mysqli_num_rows($sqlquery_consulta);
 
 echo '<span id="action_label"> <p id="userName"> Bienvenido, '.$user.'</p>'.'<p id="typeUser" >'.'('.$rol.')'.'</p>'. '<p id="userMessage"> </p> </span>' ;
 
-if($rol!="A"){ echo 'Genera tu código Q'; }
+if($rol!="A"){ echo 'Genera tu código QR'; }
 
 if($opc=="insert" && $id && $name && $type && $opc && $brand){
     if($resultado>0){
-        echo 'Ya existe un producto con el id:';  
+        echo 'Ya existe un producto con el id:\n';  
     }
-    else echo 'Se añadió el producto';
+    else echo 'Se añadió el producto \n';
         mysqli_query($connection,"INSERT INTO producto ( idProducto, nombre, categoria, marca, cantidad ) values ('$id', '$name', '$type','$brand' ,'0')");
 }
 
@@ -27,9 +27,9 @@ if($opc=="insert" && $id && $name && $type && $opc && $brand){
 if($opc=="drop" && $id){
     if( $resultado>0){
         mysqli_query($connection,"DELETE FROM producto WHERE idProducto = '$id'");
-        echo 'Se eliminó el producto';
+        echo 'Se eliminó el producto \n';
     }
-    else echo 'No se eliminó el producto, datos erroneos';
+    else echo 'No se eliminó el producto, datos erroneos \n';
 }
 
 
@@ -37,17 +37,19 @@ if($opc=="name" && $id && $aux){
     if( $resultado>0){
         echo 'Se actualizó el nombre';
     }
-    else echo 'No se actualizó el nombre, datos erroneos';
+    else echo 'No se actualizó el nombre, datos erroneos \n';
 }
 
 if($opc=="id" && $id && $aux){
     if( $resultado>0){
-        echo 'Se actualizó el ID';
+        echo 'Se actualizó el ID \n';
     }
-    else echo 'No se actualizó el ID, datos erroneos';
+    else echo 'No se actualizó el ID, datos erroneos \n';
 }
 
-else echo 'Ingrese los datos correctamente ';
+if($rol=="A"){ echo 'Ingrese los datos correctamente ';}
+
+
 
 
 ?>
